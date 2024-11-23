c68f346f677d38a83b2a259116e7b7f00973e22f
smlltt <samuel.liotta@gmail.com>
2024-11-23 11:57:00 +0100

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,23 @@ import { searchMovies } from "@/lib/tmdb";
 
 // test
 export default async function Home() {
-	const movies = await searchMovies();
+  const movies = await searchMovies();
 
-	console.log(movies);
+  console.log(movies);
 
-	return (
-		<div className="bg-background p-10">
-			<main>
-				<div className="mb-8 flex gap-2">
-					<Input placeholder="Add a movie" />
-					<Button>{"Add a movie"}</Button>
-				</div>
-				<div className="flex flex-col gap-3">
-					{movies.results.map((movie) => (
-						<MovieSearchCard key={movie.id} movie={movie} />
-					))}
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className="bg-background p-10">
+      <main>
+        <div className="mb-8 flex gap-2">
+          <Input placeholder="Add a movie" />
+          <Button>{"Add a movie"}</Button>
+        </div>
+        <div className="flex flex-col gap-3">
+          {movies.results.map((movie) => (
+            <MovieSearchCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/molecules/movie-search-card.tsx
+++ src/components/molecules/movie-search-card.tsx
@@ -1,53 +1,53 @@
 import { Card } from "@/components/ui/card";
 import { Toggle } from "@/components/ui/toggle";
 import {
-	RiEyeOffFill,
-	RiStarFill,
-	RiStarHalfLine,
-	RiStarLine,
+  RiEyeOffFill,
+  RiStarFill,
+  RiStarHalfLine,
+  RiStarLine,
 } from "react-icons/ri";
 
 export const MovieSearchCard = ({
-	movie,
+  movie,
 }: {
-	movie: {
-		id: number;
-		poster_path: string;
-		title: string;
-		release_date: string;
-		vote_average: number;
-		vote_count: number;
-	};
+  movie: {
+    id: number;
+    poster_path: string;
+    title: string;
+    release_date: string;
+    vote_average: number;
+    vote_count: number;
+  };
 }) => (
-	<Card className="flex gap-2 overflow-hidden" key={movie.id}>
-		<img
-			src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-			width={100}
-			alt="poster"
-		/>
-		<div className="flex flex-1 flex-col justify-between p-2 sm:flex-row">
-			<div className="flex flex-col justify-between">
-				<div className="flex flex-col gap-2">
-					<p className="text-xl">{movie.title}</p>
-					<p>{movie.release_date}</p>
-				</div>
-				<div className="flex items-center gap-2">
-					<p>{movie.vote_average}</p>
-					<RiStarFill className="text-yellow-400" />
-					<p>{`| ${movie.vote_count} votes`}</p>
-				</div>
-			</div>
-			<div className="flex flex-col">
-				<Toggle>
-					<RiEyeOffFill />
-				</Toggle>
-				<Toggle>Chce zobaczyć</Toggle>
-				<div className="flex gap-2">
-					<RiStarLine />
-					<RiStarHalfLine />
-					<RiStarFill />
-				</div>
-			</div>
-		</div>
-	</Card>
+  <Card className="flex gap-2 overflow-hidden" key={movie.id}>
+    <img
+      src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+      width={100}
+      alt="poster"
+    />
+    <div className="flex flex-1 flex-col justify-between p-2 sm:flex-row">
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-2">
+          <p className="text-xl">{movie.title}</p>
+          <p>{movie.release_date}</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <p>{movie.vote_average}</p>
+          <RiStarFill className="text-yellow-400" />
+          <p>{`| ${movie.vote_count} votes`}</p>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <Toggle>
+          <RiEyeOffFill />
+        </Toggle>
+        <Toggle>Chce zobaczyć</Toggle>
+        <div className="flex gap-2">
+          <RiStarLine />
+          <RiStarHalfLine />
+          <RiStarFill />
+        </div>
+      </div>
+    </div>
+  </Card>
 );
