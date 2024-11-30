d85709238bb279f31585b22b6d96681af5926ab9
smlltt <samuel.liotta@gmail.com>
2024-11-30 11:48:06 +0100

    types refactor

---
+++ src/lib/definitions.ts
@@ -0,0 +1,85 @@
+//search movies
+interface SearchMoviesResult {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieResponse {
+  page: number;
+  results: SearchMoviesResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+//search movies
+
+interface BelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface SearchMovieResult {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: BelongsToCollection | null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string | null;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -1,28 +1,4 @@
-import { Prisma } from "@prisma/client";
-
-interface SearchMoviesResult {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
-
-interface MovieResponse {
-  page: number;
-  results: SearchMoviesResult[];
-  total_pages: number;
-  total_results: number;
-}
+import { MovieResponse, SearchMovieResult } from "@/lib/definitions";
 
 const baseURL = "https://api.themoviedb.org/3";
 const options = {
@@ -51,65 +27,6 @@ export const getGenres = () =>
   ).then(
     (res) => res.json() as Promise<{ genres: { id: number; name: string }[] }>,
   );
-
-interface BelongsToCollection {
-  id: number;
-  name: string;
-  poster_path: string;
-  backdrop_path: string;
-}
-
-interface Genre {
-  id: number;
-  name: string;
-}
-
-interface ProductionCompany {
-  id: number;
-  logo_path: string | null;
-  name: string;
-  origin_country: string;
-}
-
-interface ProductionCountry {
-  iso_3166_1: string;
-  name: string;
-}
-
-interface SpokenLanguage {
-  english_name: string;
-  iso_639_1: string;
-  name: string;
-}
-
-interface SearchMovieResult {
-  adult: boolean;
-  backdrop_path: string;
-  belongs_to_collection: BelongsToCollection | null;
-  budget: number;
-  genres: Genre[];
-  homepage: string;
-  id: number;
-  imdb_id: string;
-  origin_country: string[];
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  production_companies: ProductionCompany[];
-  production_countries: ProductionCountry[];
-  release_date: string; // ISO date string
-  revenue: number;
-  runtime: number | null;
-  spoken_languages: SpokenLanguage[];
-  status: string;
-  tagline: string | null;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
 
 export const searchMovie = (id: number) =>
   fetch(`${baseURL}/movie/${id}`, options).then(
