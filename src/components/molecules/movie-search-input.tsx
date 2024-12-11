b03512c8b8ed6570d3158d8b100cce05507afd50
Zachariasz Strus <zachariasz.strus@gmail.com>
2024-12-11 22:04:38 +0100

    add transition to search

--- src/components/molecules/movie-search-input.tsx
+++ src/components/molecules/movie-search-input.tsx
@@ -7,13 +7,17 @@ import {
   type JSX,
   useCallback,
   useEffect,
+  useRef,
   useState,
+  useTransition,
 } from "react";
 import { useDebounce } from "use-debounce";
+import { Spinner } from "../ui/spinner";
 
 export const SEARCH_QUERY_KEY = "search";
 
-export const MovieSearchInput = () => {
+export const MovieSearchInput = (): JSX.Element => {
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -21,23 +25,32 @@ export const MovieSearchInput = () => {
   const [searchText, setSerchText] = useState(
     searchParams.get(SEARCH_QUERY_KEY) || "",
   );
-  const [debouncedValue] = useDebounce(searchText, 500);
+  const [debouncedValue] = useDebounce(searchText, 300);
 
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSerchText(e.target.value);
   }, []);
 
+  const isFirstRender = useRef(true);
   useEffect(() => {
-    if (debouncedValue) {
-      router.push(
-        `${pathname}?${new URLSearchParams({ [SEARCH_QUERY_KEY]: debouncedValue })}`,
-      );
-    } else {
-      router.push(pathname);
+    if (!isFirstRender.current) {
+      startTransition(() => {
+        if (debouncedValue) {
+          router.replace(
+            `${pathname}?${new URLSearchParams({ [SEARCH_QUERY_KEY]: debouncedValue })}`,
+          );
+        } else {
+          router.replace(pathname);
+        }
+      });
     }
+    isFirstRender.current = false;
   }, [debouncedValue, pathname, router]);
 
   return (
-    <Input placeholder="Add a movie" value={searchText} onChange={onChange} />
+    <div className="relative">
+      <Input placeholder="Add a movie" value={searchText} onChange={onChange} />
+      {isPending && <Spinner className="absolute top-2.5 right-2.5" />}
+    </div>
   );
 };
